29976a606166812bdc324784472ca3164cde0c95
Masataka Shida <ar.tec.masa@gmail.com>
2020-08-14 10:39:51 +0900

    postgresと接続できるようにする

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM golang:1.14.7
 WORKDIR /go/src/app
 COPY . .
 
+RUN go get github.com/lib/pq
 RUN go get -d -v ./...
 RUN go install -v ./...
 
--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
+	_ "github.com/lib/pq"
+)
 
 func main() {
 	fmt.Println("hello world")
+
+	dsn := os.Getenv("DSN")
+	db, err := sql.Open("postgres", dsn)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
 }
